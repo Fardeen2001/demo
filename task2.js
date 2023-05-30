ce786f87a431bc0024cf722e82869ca952a01896
Fardeen2001 <1si20et403@sit.ac.in>
2023-05-30 13:42:57 +0530

    commit

---
+++ task2.js
@@ -0,0 +1,12 @@
+// let list=document.getElementsByClassName("list-group-item");
+// list[2].style.background="green";
+
+// for(let i=0;i<list.length;i++){
+//     list[i].style.fontWeight="bold"
+// }
+
+let li=document.getElementsByTagName("li");
+console.log(li);
+li[4].style.border="3px solid #4f4f4f"
+li[4].style.color="green"
+li[4].style.listStyle="none"
