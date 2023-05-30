60e263e8abc22547676f24ef495fdf908de7719a
Fardeen2001 <1si20et403@sit.ac.in>
2023-05-30 11:16:38 +0530

    comitted

---
+++ dom.js
@@ -0,0 +1,16 @@
+// console.dir(document)
+// console.log(document.domain);
+// console.log(document.title)
+// document.title="fardeen"
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+let header=document.getElementById("header-title");
+header.style.borderBottom="3px solid #000";
+// let header=document.getElementById("main-header");
+// header.style.border="3px solid black"
+header.style.fontWeight="bold"
+header.style.color="green"
